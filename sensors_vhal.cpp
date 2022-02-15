d3af0da7022097e7b0df9adb96382b92ed7d9074
Jaikrishna, Nemallapudi <nemallapudi.jaikrishna@intel.com>
2022-02-15 14:45:16 +0530

    Fix to check sensor handle has valid data.
    Added check to know sensor handle received from
    framework is correct.
    Signed-off-by: Jaikrishna, Nemallapudi <nemallapudi.jaikrishna@intel.com>

--- sensors_vhal.cpp
+++ sensors_vhal.cpp
@@ -486,6 +486,9 @@ int SensorDevice::sensor_device_set_delay(int handle, int64_t ns) {
 }
 
 int SensorDevice::sensor_device_flush(int handle) {
+    if (handle >= sensor_device_flush)
+        return 0;
+
     m_mutex.lock();
     if ((m_pending_sensors & (1U << handle)) && m_sensors[handle].type == SENSOR_TYPE_META_DATA) {
         (m_flush_count[handle])++;
