47f114f4acf8b4d6308c70cef019da34dd204879
Jaikrishna, Nemallapudi <nemallapudi.jaikrishna@intel.com>
2022-12-05 14:36:08 +0530

    Update copyright and license information
    Tracked-On: OAM-105139
    Signed-off-by: Jaikrishna, Nemallapudi <nemallapudi.jaikrishna@intel.com>

--- lib/include/Sensor.h
+++ lib/include/Sensor.h
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef ANDROID_HARDWARE_SENSORS_V2_X_SENSOR_H
